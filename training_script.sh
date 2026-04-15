2d6a1aaf2a3bf2fe5483bf0b08e849fa679052e7
Kai Chen Chai <65500723+kaichenchai@users.noreply.github.com>
2026-04-16 00:23:37 +0800

    fix(hsic-loss): updated training scripts, added new configurations

--- training_script.sh
+++ training_script.sh
@@ -1,5 +1,7 @@
 # python training/train.py --detector_path ./training/config/detector/effort_ce.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
-python training/train.py --detector_path ./training/config/detector/effort_ce_orthogonal.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
-python training/train.py --detector_path ./training/config/detector/effort_ce_hsic.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
-python training/train.py --detector_path ./training/config/detector/effort_ce_hsic_orthogonal.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
+# python training/train.py --detector_path ./training/config/detector/effort_ce_orthogonal.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
+# python training/train.py --detector_path ./training/config/detector/effort_ce_hsic.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
+# python training/train.py --detector_path ./training/config/detector/effort_ce_hsic_orthogonal.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
+python training/train.py --detector_path ./training/config/detector/effort_ce_orthogonal_weight.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
+python training/train.py --detector_path ./training/config/detector/effort_ce_hsic_orthogonal_weight.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
 sudo shutdown now
