ddd630a3e1bbc09f40754c4a952772935668aa28
Kai Chen Chai <65500723+kaichenchai@users.noreply.github.com>
2026-04-22 19:19:51 +0800

    feat(counterfactual-loss): update training script

--- training_script.sh
+++ training_script.sh
@@ -1,7 +1,9 @@
-python training/train.py --detector_path ./training/config/detector/effort_ce.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
-python training/train.py --detector_path ./training/config/detector/effort_ce_orthogonal.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
-python training/train.py --detector_path ./training/config/detector/effort_ce_hsic.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
-python training/train.py --detector_path ./training/config/detector/effort_ce_hsic_orthogonal.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
+# python training/train.py --detector_path ./training/config/detector/effort_ce.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
+# python training/train.py --detector_path ./training/config/detector/effort_ce_orthogonal.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
+# python training/train.py --detector_path ./training/config/detector/effort_ce_hsic.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
+# python training/train.py --detector_path ./training/config/detector/effort_ce_hsic_orthogonal.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
 # python training/train.py --detector_path ./training/config/detector/effort_ce_orthogonal_weight.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
 # python training/train.py --detector_path ./training/config/detector/effort_ce_hsic_orthogonal_weight.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
+python training/train.py --detector_path ./training/config/detector/effort_ce_counterfactual.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
+python training/train.py --detector_path ./training/config/detector/effort_ce_masked_counterfactual.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
 sudo shutdown now
