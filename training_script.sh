1fd8757f9424a7209913e37fa57702ab2e4d5041
Kai Chen Chai <65500723+kaichenchai@users.noreply.github.com>
2026-04-23 14:46:21 +0800

    feat(counterfactual-loss): update training script to not save checkpoints and features

--- training_script.sh
+++ training_script.sh
@@ -5,7 +5,7 @@
 # python training/train.py --detector_path ./training/config/detector/effort_ce_orthogonal_weight.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
 # python training/train.py --detector_path ./training/config/detector/effort_ce_hsic_orthogonal_weight.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
 # python training/train.py --detector_path ./training/config/detector/effort_ce_counterfactual.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
-python training/train.py --detector_path ./training/config/detector/effort_ce_masked_counterfactual.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
+python training/train.py --detector_path ./training/config/detector/effort_ce_masked_counterfactual.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1" --no-save_ckpt --no-save_feat
 # python training/train.py --detector_path ./training/config/detector/effort_ce_hsic_orthogonal_masked_counterfactual.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
-python training/train.py --detector_path ./training/config/detector/effort_ce_masked_counterfactual_backbone.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
+python training/train.py --detector_path ./training/config/detector/effort_ce_masked_counterfactual_backbone.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1" --no-save_ckpt --no-save_feat
 sudo shutdown now
