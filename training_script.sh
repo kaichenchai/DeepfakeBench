934f0b4018699faa78cdf49e94fc6729bb5353f7
Kai Chen Chai <65500723+kaichenchai@users.noreply.github.com>
2026-05-04 18:57:50 +0800

    feat(counterfactual-loss): update training scripts, update custom detector implementation to roll back loss

--- training_script.sh
+++ training_script.sh
@@ -12,5 +12,9 @@ source .venv/bin/activate
 # python training/train.py --detector_path ./training/config/detector/effort_ce_counterfactual.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
 # python training/train.py --detector_path ./training/config/detector/effort_ce_masked_counterfactual.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1" --no-save_ckpt --no-save_feat
 # python training/train.py --detector_path ./training/config/detector/effort_ce_hsic_orthogonal_masked_counterfactual.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1"
-python training/train.py --detector_path ./training/config/detector/effort_ce_masked_counterfactual_backbone.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1" --no-save_ckpt --no-save_feat
+# python training/train.py --detector_path ./training/config/detector/effort_ce_masked_counterfactual_backbone.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1" --no-save_ckpt --no-save_feat
+python training/train.py --detector_path ./training/config/detector/effort_ce_hsic_masked_counterfactual_backbone.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1" --no-save_ckpt --no-save_feat
+python training/train.py --detector_path ./training/config/detector/effort_ce_hsic_orthogonal_masked_counterfactual_backbone.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1" --no-save_ckpt --no-save_feat
+python training/train.py --detector_path ./training/config/detector/effort_ce_hsic_orthogonal_weight_masked_counterfactual_backbone.yaml  --train_dataset "Celeb-DF-v1" --test_dataset "Celeb-DF-v1" --no-save_ckpt --no-save_feat
+
 sudo shutdown now
